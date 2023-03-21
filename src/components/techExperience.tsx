5b83d7c6c6004dd8f9776b400b3537a0b61cd1f3
IanConceicao <IanCon234@gmail.com>
2023-03-21 15:44:56 -0700

    Color changes

--- src/components/techExperience.tsx
+++ src/components/techExperience.tsx
@@ -7,7 +7,7 @@ export default function TechExperience({ title, techs }: techExperienceProps) {
   return (
     <>
       <p className="font-book text-primaryText text-lg mb-2">{title}</p>
-      <div className="columns-[9.375rem] space-y-2">
+      <div className="columns-2 md:columns-[9.375rem] space-y-2">
         {techs.map((tech) => (
           <p
             key={tech}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "#e5e5e5",
-        primaryHeader: "#5D4063",
-        altPrimaryHeader: "#fca311",
-        secondaryHeader: "#454F6A",
+        background: "#F2F7FF",
+        primaryHeader: "#0f58d6",
+        altPrimaryHeader: "#e2619f",
+        secondaryHeader: "#10316B",
         primaryText: "#14213D",
+        // background: "#e5e5e5",
+        // primaryHeader: "#5D4063",
+        // altPrimaryHeader: "#fca311",
+        // secondaryHeader: "#454F6A",
+        // primaryText: "#14213D",
       },
       fontFamily: {
         book: ["StolzlBook"],
