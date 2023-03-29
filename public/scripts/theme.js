a0bec00443b9ddaf8496c9cebaba50552a6b9f6c
IanConceicao <IanCon234@gmail.com>
2023-03-28 17:04:18 -0700

    Added a 'system' option for themes

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,11 +1,14 @@
 if (
+  // Dark theme, or no theme and device in dark mode
   localStorage.theme === "dark" ||
   (!("theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.setAttribute("data-theme", "dark");
 } else if (localStorage.theme == "vintage") {
+  // Vintage theme
   document.documentElement.setAttribute("data-theme", "vintage");
 } else {
+  // Default
   document.documentElement.setAttribute("data-theme", "default");
 }
--- src/components/themePicker.tsx
+++ src/components/themePicker.tsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 
 export default function ThemePicker() {
-  const themes = ["Default", "Dark", "Vintage"];
+  const themes: { display: string; internal: string }[] = [
+    { display: "System", internal: "system" },
+    { display: "Light", internal: "default" },
+    { display: "Dark", internal: "dark" },
+    { display: "Vintage", internal: "vintage" },
+  ];
   const [currentTheme, setCurrentTheme] = useState("");
 
   useEffect(() => {
     // Set theme state on page load
-    setCurrentTheme(
-      document.documentElement.getAttribute("data-theme") ||
-        localStorage.theme ||
-        "Default"
-    );
+    setCurrentTheme(localStorage.theme || "system");
   }, []);
 
   useEffect(() => {
     // Change DOM and local storage on theme change
-    if (currentTheme) {
-      document.documentElement.setAttribute(
-        "data-theme",
-        currentTheme.toLowerCase()
-      );
+
+    if (currentTheme === "system") {
+      localStorage.removeItem("theme");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-theme", "default");
+      }
+    } else if (currentTheme) {
+      document.documentElement.setAttribute("data-theme", currentTheme);
       localStorage.setItem("theme", currentTheme);
     }
   }, [currentTheme]);
@@ -28,17 +34,17 @@ export default function ThemePicker() {
     <div className="flex gap-x-2 mb-3">
       {themes.map((theme) => (
         <button
-          key={theme}
+          key={theme.internal}
           type="button"
           className={`text-center text-sm px-2 py-1 border 
            rounded-full ${
-             currentTheme.toLowerCase() == theme.toLowerCase()
+             currentTheme == theme.internal
                ? "text-select border-select"
                : "text-primaryHeader text-hover-effect border-primaryHeader hover:border-select"
            }`}
-          onClick={() => setCurrentTheme(theme)}
+          onClick={() => setCurrentTheme(theme.internal)}
         >
-          {theme}
+          {theme.display}
         </button>
       ))}
     </div>
